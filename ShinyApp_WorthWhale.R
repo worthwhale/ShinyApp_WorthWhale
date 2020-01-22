2b66d68ca4179a7c16c254311c15c761d5913900
Callie Steffen <calliemsteffen@gmail.com>
2020-01-22 09:31:07 -0800

    starting shiny code

---
+++ ShinyApp_WorthWhale.R
@@ -0,0 +1,46 @@
+library(tidyverse)
+library(shiny)
+library(shinythemes)
+library(here)
+library(leaflet)
+library(ggmap)
+
+#Read in cluster_data.csv
+
+clusters <-read_csv("clusters.csv")
+
+# Create my user interface
+
+ui <- fluidPage(
+  theme = shinytheme("flatly"),
+  titlePanel("Sperm Whale Distrubution in Dominica"),
+  sidebarLayout(
+    sidebarPanel("My widgets are here",
+                 selectInput(inputId = "date_select",
+                             label = "Choose a Year:",
+                             choices = unique(clusters$datetime)
+                 )
+    ),
+    mainPanel("My outputs are here!",
+              tableOutput(outputId = "sighting_table")
+    )
+  )
+)
+
+server <- function(input, output) {
+  
+  date_select <- reactive({
+    clusters %>%
+      select(datetime, lat, lon)
+    
+  })
+  
+  output$sighting_table <- renderTable({
+  date_select()
+    
+  })
+  
+}
+
+
+shinyApp(ui = ui, server = server)
