bdfa240701e7b040a8c5190e540d369c64df3092
Callie Steffen <calliemsteffen@gmail.com>
2020-02-03 11:34:50 -0800

    radio button for vessel category

--- ShinyApp_WorthWhale.R
+++ ShinyApp_WorthWhale.R
@@ -8,20 +8,19 @@ library(ggmap)
 #Read in cluster_data.csv
 
 clusters <-read_csv("clusters.csv")
+vessels <- read_csv("shiny_vessels.csv")
 
 # Create my user interface
 
 ui <- fluidPage(
   theme = shinytheme("flatly"),
-  titlePanel("Sperm Whale Distrubution in Dominica"),
+  titlePanel("Sperm Whale and Vessel Traffic Distrubution in Dominica"),
   sidebarLayout(
     sidebarPanel("My widgets are here",
-                 selectInput(inputId = "date_select",
-                             label = "Choose a Year:",
-                             choices = unique(clusters$datetime)
-                 )
-    ),
-    mainPanel("My outputs are here!",
+                 radioButtons(inputId = "category",
+                              label = "Choose a Vessel Category:",
+                              choices = c("cruiseship","merchant","high_speed_ferry","passenger"))),
+               mainPanel("My outputs are here!",
               leafletOutput("sighting_map"))
               
     
